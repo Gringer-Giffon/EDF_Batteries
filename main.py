730ce0d48b85a0f5769dfb2fecc78ae2b98fae94
Etienne <guegan.etienne@gmail.com>
2025-01-28 20:16:18 +0100

    function to plot voltage current and step for specific cell and test

---
+++ main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+directory = f"./cells_data"
+
+csv_files = [f for f in os.listdir(directory)] #extracts all csv file names in directory
+
+#extract function to extract all data
+
+def plot_test(cell, test):
+    '''
+    Parameters: cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Plots voltage current and step for given test
+    Does not show plot
+
+    Returns none
+    '''
+    file = [pd.read_csv(os.path.join(directory,f)) for f in csv_files if '_'+(str(cell).upper())+"_" in f and str(test) in f]
+    data = pd.concat(file) #dataframe of corresponding csv data
+    
+    if file == []:
+        print("No test found for given cell and test. Cell entry must be C/c or D/d")
+        return None
+    
+    # Data extraction from dataframe
+    time = data["Total Time"]
+    current = data["Current"]
+    voltage = data["Voltage"]
+    step = data["Step"]
+
+    # Plotting
+    fig, axs = plt.subplots(3,1)
+    
+    fig.suptitle("Cell: "+cell.upper()+",test: "+test) #main title
+    
+    axs[0].plot(time, current,"g")
+    axs[0].set_title("Current")
+    axs[1].plot(time, voltage,"g")
+    axs[1].set_title("Voltage")
+    axs[2].plot(time, step, "g")
+    axs[2].set_title("Step")
+
+    plt.subplots_adjust(hspace=1) #adjust space between plots
+
+    return None
+
+plot_test("d","01")
+plot_test("C","00")
+plt.show()
