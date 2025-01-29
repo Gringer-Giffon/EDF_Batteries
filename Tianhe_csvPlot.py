de1c3187e5ea5ba3b8a57a6e4566df6e17c0034e
Dhanny-Lee <139756600+Dhanny-Lee@users.noreply.github.com>
2025-01-29 09:15:36 +0100

    Extract data with repect to steps

--- Tianhe_csvPlot.py
+++ Tianhe_csvPlot.py
@@ -27,12 +27,36 @@ def plotAll(dfs, name, start=None, end=None):       # Plot everything with repse
         df = extract(df, start, end)
         plt.plot(df['Total Time'], df[name])
 
+def locate(df, step, pattern, offset=0):        # Extract the df with respect to steps
+    mask = df['Step'] == step
+    df['start'] = mask& ~mask.shift(1, fill_value=False)
+    df['end'] = mask& ~mask.shift(-1, fill_value=False)
+    start_indices = df.index[df['start']].tolist()
+    end_indices = df.index[df['end']].tolist()
+    return df['Total Time'][start_indices[pattern]-offset], df['Total Time'][end_indices[pattern]]
+
 if __name__ == "__main__":
-    plt.subplot(2,2,1)
-    plotAll(dfd, 'Voltage', 0, 35000)
+    start, end1 = locate(dfd[0], 6, 1, 1)
+    start1, end = locate(dfd[0], 7, 1)
+    
     plt.subplot(2,2,2)
-    plotAll(dfd, 'Current', 0, 35000)
+    #plotAll(dfd, 'Voltage', start, 42000)
+    plt.plot(extract(dfd[0], start, end)['Total Time'], extract(dfd[0], start, end)['Voltage'])
+    
+    plt.subplot(2,2,1)
+    #plotAll(dfd, 'Current', 36000, 42000)
+    plt.plot(extract(dfd[0], start, end)['Total Time'], extract(dfd[0], start, end)['Current'])
+    
     plt.subplot(2,2,3)
-    plotAll(dfd, 'Step', 0, 35000)
+    #plotAll(dfd, 'Step', 36000, 42000)
+    plt.plot(extract(dfd[0], start, end)['Total Time'], extract(dfd[0], start, end)['Step'])
+
+    '''
+    plt.text(37300, 7.3, "step7:\nan instant and\nrapid discharge", fontsize=12, color="red")
+    plt.text(39000, 9.3, "step9:\nan instant and\nrapid charge", fontsize=12, color="red")
+    
+    plt.text(41000, 10.3, "step10:\na continous discharge", fontsize=12, color="blue")
+    '''
 
     plt.show()
+    
