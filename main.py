7ad2cf51adfeab9be744a79f722c2dbf1a1350ed
Etienne <guegan.etienne@gmail.com>
2025-01-29 08:57:53 +0100

    extract data function

--- main.py
+++ main.py
@@ -20,19 +20,13 @@ def plot_test(cell, test):
 
     Returns none
     '''
-    file = [pd.read_csv(os.path.join(directory, f))
-            for f in csv_files if '_'+(str(cell).upper())+"_" in f and str(test) in f]
-    data = pd.concat(file)  # dataframe of corresponding csv data
 
-    if file == []:
-        print("No test found for given cell and test. Cell entry must be C/c or D/d")
-        return None
-
+    plot_data = extract(cell, test)
     # Data extraction from dataframe
-    time = data["Total Time"]
-    current = data["Current"]
-    voltage = data["Voltage"]
-    step = data["Step"]
+    time = plot_data[0]
+    current = plot_data[1]
+    voltage = plot_data[2]
+    step = plot_data[3]
 
     # Plotting
     fig, axs = plt.subplots(3, 1)
@@ -49,6 +43,31 @@ def plot_test(cell, test):
     plt.subplots_adjust(hspace=1)  # adjust space between plots
 
     return None
+
+
+def extract(cell, test):
+    '''
+    Parameters : cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Extracts raw data from csv files corresponding to given cell and test
+    Returns time, current, voltage, step columns as dataframe columns
+    '''
+
+    file = [pd.read_csv(os.path.join(directory, f))
+            for f in csv_files if '_'+(str(cell).upper())+"_" in f and str(test) in f]
+    data = pd.concat(file)  # dataframe of corresponding csv data
+
+    if file == []:
+        print("No test found for given cell and test. Cell entry must be C/c or D/d")
+        return None
+
+    # Data extraction from dataframe
+    time = data["Total Time"]
+    current = data["Current"]
+    voltage = data["Voltage"]
+    step = data["Step"]
+
+    return time, current, voltage, step
 
 
 if __name__ == '__main__':
