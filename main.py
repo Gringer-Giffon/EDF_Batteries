e3e7e0cdc5dfd9858d26880f09fdf9e8188658ad
Etienne <guegan.etienne@gmail.com>
2025-01-29 09:24:30 +0100

    new plot_step and extract_step functions

--- main.py
+++ main.py
@@ -64,6 +64,50 @@ def extract(cell, test):
     return data
 
 
+def extract_step(first,second,cell,test):
+    '''
+    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Returns dataframe for given step interval
+    '''
+
+    data = extract(cell,test)
+    step_data = data[data["Step"].isin(list(range(first,second+1)))]
+    return step_data
+
+
+def plot_step(first,second,cell,test):
+    '''
+    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Plots voltage current and step for given step interval
+    '''
+
+    # Extracting data
+    step_data = extract_step(first,second,cell,test)
+    time = step_data["Total Time"]
+    current = step_data["Current"]
+    voltage = step_data["Voltage"]
+    step = step_data["Step"]
+
+    # Plotting
+    fig, axs = plt.subplots(3, 1)
+
+    fig.suptitle("Cell: "+cell.upper()+",test: "+test)  # main title
+
+    axs[0].plot(time, current, "g")
+    axs[0].set_title("Current")
+    axs[1].plot(time, voltage, "g")
+    axs[1].set_title("Voltage")
+    axs[2].plot(time, step, "g")
+    axs[2].set_title("Step")
+
+    plt.subplots_adjust(hspace=1)  # adjust space between plots
+
+    return None
+
 if __name__ == '__main__':
     plot_test("C", "01")
     plt.show()
+
+
