1c168703a71a23ea2f64a3e960c3cb13ad577b74
Etienne <guegan.etienne@gmail.com>
2025-01-29 08:49:17 +0100

    small changes in personal file

--- cell_plotting_tests_etienne.py
+++ cell_plotting_tests_etienne.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import main
 
 folderPath = f'./cells_data'
 
-dataframes = [pd.read_csv(file) for file in os.listdir(folderPath)]
+# dataframes = [pd.read_csv(file) for file in os.listdir(folderPath)]
 
 """
 time = data["Total Time"]
@@ -14,9 +15,5 @@ voltage = data["Voltage"]
 step = data["Step"]
 """
 
-fig, axs = plt.subplots(3, 1)
-axs[0].plot(time, current)
-axs[1].plot(time, voltage)
-axs[2].plot(time, step)
-
+main.plot_test("C", "01")
 plt.show()
