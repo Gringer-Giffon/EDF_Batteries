344c603cc52ce8f43613826928738935ecec0b90
Etienne <guegan.etienne@gmail.com>
2025-01-30 19:10:04 +0100

    moved functions to annex

--- annex.py
+++ annex.py
@@ -1,1 +1,69 @@
+import data as dt
+import plot as pt
 
+def ocv_voltage():
+    soc_d_t_list = soc_d_time("01")
+    print(soc_d_t_list)
+    soc_ocv_v = {0.05*i for i in range(0, 21)}
+    for element in soc_ocv_v:
+        for pair in soc_d_t_list:
+            if element == pair[0]:
+                soc_ocv_v[element] = dt.extract("D", "01")["Voltage"][dt.extract("D", "01")[
+                    "Total Time"] == pair[1]]
+    print(soc_ocv_v)
+    return None
+
+
+def soc_d(test):
+    '''
+    Parameters: test (string) in the form 00, 01, etc..
+
+    Calculates the state of charge of D cell at a given time for the discharge at the end of the test
+    Returns list of SOC values in the full discharge phase for cell D
+    '''
+
+    data = dt.extract_step(21, 23, "D", test)
+
+    # Calculate I and t
+    I = abs(data["Current"].mean())
+    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
+
+    # Calculate Q remaining and Q available
+    Q_remaining = I*t/3600
+
+    Q_available = [Q_remaining - I*(data["Total Time"].iloc[i] -
+                                    data["Total Time"].iloc[0])/3600 for i in range(len(data["Total Time"]))]
+
+    SOC = [Q_available[i]/Q_remaining for i in range(len(data["Total Time"]))]
+
+    return SOC
+
+def soc_d_time(test):
+    '''
+    Parameters: test (string) in the form 00, 01, etc..
+
+    Calculates the state of charge of D cell at a given time for the discharge at the end of the test
+    Returns list of SOC values
+    '''
+
+    data = dt.extract_step(21, 24, "D", test)
+
+    # Calculate I and t
+    I = abs(data["Current"].mean())
+    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
+
+    # Calculate Q remaining and Q available
+    Q_remaining = I*t/3600
+
+    Q_available = [(Q_remaining - I*(data["Total Time"].iloc[i] -
+                                     data["Total Time"].iloc[0])/3600, data["Total Time"].iloc[i]) for i in range(len(data["Total Time"]))]
+
+    SOC = [(Q_available[i][0]/Q_remaining, data["Total Time"].iloc[i])
+           for i in range(len(data["Total Time"]))]
+
+    """
+    soc_voltage_dict = {
+        data["Voltage"].iloc[i]: Q_available[i] / Q_remaining
+        for i in range(len(data["Total Time"]))}
+    """
+    return SOC
--- data.py
+++ data.py
@@ -79,37 +79,6 @@ def q_initial(cell):
     return Q_remaining
 
 
-def soc_d_time(test):
-    '''
-    Parameters: test (string) in the form 00, 01, etc..
-
-    Calculates the state of charge of D cell at a given time for the discharge at the end of the test
-    Returns list of SOC values
-    '''
-
-    data = extract_step(21, 24, "D", test)
-
-    # Calculate I and t
-    I = abs(data["Current"].mean())
-    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
-
-    # Calculate Q remaining and Q available
-    Q_remaining = I*t/3600
-
-    Q_available = [(Q_remaining - I*(data["Total Time"].iloc[i] -
-                                     data["Total Time"].iloc[0])/3600, data["Total Time"].iloc[i]) for i in range(len(data["Total Time"]))]
-
-    SOC = [(Q_available[i][0]/Q_remaining, data["Total Time"].iloc[i])
-           for i in range(len(data["Total Time"]))]
-
-    """
-    soc_voltage_dict = {
-        data["Voltage"].iloc[i]: Q_available[i] / Q_remaining
-        for i in range(len(data["Total Time"]))}
-    """
-    return SOC
-
-
 def soc_full_c(test):
     '''
     Parameters: test (string) in the form 00, 01, etc..
@@ -160,31 +129,6 @@ def soc_full_d(test):
     return SOC
 
 
-def soc_d(test):
-    '''
-    Parameters: test (string) in the form 00, 01, etc..
-
-    Calculates the state of charge of D cell at a given time for the discharge at the end of the test
-    Returns list of SOC values in the full discharge phase for cell D
-    '''
-
-    data = extract_step(21, 23, "D", test)
-
-    # Calculate I and t
-    I = abs(data["Current"].mean())
-    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
-
-    # Calculate Q remaining and Q available
-    Q_remaining = I*t/3600
-
-    Q_available = [Q_remaining - I*(data["Total Time"].iloc[i] -
-                                    data["Total Time"].iloc[0])/3600 for i in range(len(data["Total Time"]))]
-
-    SOC = [Q_available[i]/Q_remaining for i in range(len(data["Total Time"]))]
-
-    return SOC
-
-
 def soh(cell, test):
     '''
     Parameters: test (string) in the form 00, 01, etc..
@@ -214,19 +158,6 @@ def soh(cell, test):
     return SOH
 
 
-def ocv_voltage():
-    soc_d_t_list = soc_d_time("01")
-    print(soc_d_t_list)
-    soc_ocv_v = {0.05*i for i in range(0, 21)}
-    for element in soc_ocv_v:
-        for pair in soc_d_t_list:
-            if element == pair[0]:
-                soc_ocv_v[element] = extract("D", "01")["Voltage"][extract("D", "01")[
-                    "Total Time"] == pair[1]]
-    print(soc_ocv_v)
-    return None
-
-
 def find_OCV(cell, test):
     """
     Parameters: cell (str) C or D, 
@@ -247,10 +178,8 @@ def find_OCV(cell, test):
 
 if __name__ == "__main__":
     print(find_OCV("D", "01"))
-    ocv_voltage()
-
-
     
+
     # look at C
     # make stuff available for cell C
     # find R0
