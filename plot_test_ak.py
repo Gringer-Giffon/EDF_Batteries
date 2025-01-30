f2604482a476a43d7badc094e51ebb0b5b7b5f50
Kuroda Akira <akirakuroda47@gmail.com>
2025-01-30 09:33:59 +0100

    Finding OCV update on cell D

--- plot_test_ak.py
+++ plot_test_ak.py
@@ -1,73 +1,293 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import os
-import main
+
+directory = f"./cells_data"
+
+# extracts all csv file names in directory
+csv_files = [f for f in os.listdir(directory)]
+
+# extract function to extract all data
 
 
-folderPath = f'./cells_data'
+def plot_test(cell, test):
+    '''
+    Parameters: cell (string) C or D, test (string) in the form 00, 01, etc..
 
-# dataframes = [pd.read_csv(file) for file in os.listdir(folderPath)]
+    Plots voltage current and step for given test
+    Does not show plot
 
-"""
-time = data["Total Time"]
-current = data["Current"]
-voltage = data["Voltage"]
-step = data["Step"]
-"""
+    Returns none
+    '''
 
-print(main.extract("C", "01")["Step"])
+    plot_data = extract(cell, test)
+    # Data extraction from dataframe
+    time = plot_data["Total Time"]
+    current = plot_data["Current"]
+    voltage = plot_data["Voltage"]
+    step = plot_data["Step"]
+
+    # Plotting
+    fig, axs = plt.subplots(3, 1)
+
+    fig.suptitle("Cell: "+cell.upper()+",test: "+test)  # main title
+
+    axs[0].plot(time, current, "g")
+    axs[0].set_title("Current")
+    axs[1].plot(time, voltage, "g")
+    axs[1].set_title("Voltage")
+    axs[2].plot(time, step, "g")
+    axs[2].set_title("Step")
+
+    plt.subplots_adjust(hspace=1)  # adjust space between plots
+
+    return None
 
 
-# Q remaining is the integral of I on a full discharge
-# Q available at a time tis the difference between the Q remaining and the integral of Q up to t
+def extract(cell, test):
+    '''
+    Parameters : cell (string) C or D, test (string) in the form 00, 01, etc..
 
-def soc_d(test):
-    data = main.extract_step(21, 23, "D", test)
-    
-    full_data = main.extract("D", test)
-    full_t = full_data["Total Time"]
-    full_voltage = full_data["Voltage"]
-    
-    
-    
-    
+    Extracts raw data from csv files corresponding to given cell and test
+    Returns dataframe of extracted data
+    '''
+
+    file = [pd.read_csv(os.path.join(directory, f))
+            for f in csv_files if '_'+(str(cell).upper())+"_" in f and str(test) in f]
+    data = pd.concat(file)  # dataframe of corresponding csv data
+
+    if file == []:
+        print("No test found for given cell and test. Cell entry must be C/c or D/d")
+        return None
+
+    return data
+
+
+def extract_all_steps(first, second, cell, test):
+    '''
+    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Returns dataframe for given step interval, does not remove duplicate step sequences
+    '''
+
+    data = extract(cell, test)
+    step_data = data[data["Step"].isin(list(range(first, second+1)))]
+    return step_data
+
+
+def extract_step(first, second, cell, test):
+    '''
+    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Returns dataframe for given step interval
+    '''
+
+    data = extract(cell, test)
+    step_data = data[data["Step"].isin(list(range(first, second+1)))]
+
+    # Remove duplicate step sequences
+    step_indices = step_data.index
+    for i in range(1, len(step_indices)):
+        # Check for breaks in the sequence
+        if step_indices[i] != step_indices[i - 1] + 1:
+            # Keep only the first block
+            step_data = step_data.loc[step_indices[:i]]
+            break
+    return step_data
+
+
+def plot_step(first, second, cell, test):
+    '''
+    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
+
+    Plots voltage current and step for given step interval
+    Returns None
+    '''
+
+    # Extracting data
+    step_data = extract_step(first, second, cell, test)
+    time = step_data["Total Time"]
+    current = step_data["Current"]
+    voltage = step_data["Voltage"]
+    step = step_data["Step"]
+
+    # Plotting
+    fig, axs = plt.subplots(3, 1)
+
+    fig.suptitle("Cell: "+cell.upper()+",test: "+test)  # main title
+
+    axs[0].plot(time, current, "g")
+    axs[0].set_title("Current")
+    axs[1].plot(time, voltage, "g")
+    axs[1].set_title("Voltage")
+    axs[2].plot(time, step, "g")
+    axs[2].set_title("Step")
+
+    plt.subplots_adjust(hspace=1)  # adjust space between plots
+
+    return None
+
+
+def extract_OCV_D():
+    '''
+    USELESS FUNCTION
+
+    Takes average of OCV for each test on cell D
+
+    Returns dictionary of test number and average OCV
+    '''
+    extract_data = {test: 0 for test in range(0, 13+1)}
+    for element in extract_data:
+        print(element)
+        average = 0
+        if element < 10:
+            step_data = extract_all_steps(
+                5, 7, "D", "0"+str(element))[extract_all_steps(5, 7, "D", "0"+str(element))["Step"] == 7]
+            print(step_data)
+        else:
+            step_data = extract_all_steps(5, 7, "D", str(element))[
+                extract_all_steps(5, 7, "D", str(element))["Step"] == 7]
+        for value in step_data["Voltage"]:
+            average += value
+        extract_data[element] = average/len(step_data["Voltage"])
+    return extract_data
+    '''
+    extract_data = {test:0 for test in range(0,13+1)}
+    for element in extract_data:
+        step_data = extract_step(5, 7, "D", element)
+        extract_data[element] = step_data["Voltage"].iloc[-1]
+    return extract_data
+    '''
+
+
+def q_initial_d_precise():
+    '''
+    Returns initial charge for D cell
+    '''
+    voltage_at_time = extract("D", "00")[(extract("D", "00")[
+        "Total Time"] >= 123658.7) & (extract("D", "00")["Total Time"] <= 142474.6)]
+    I = abs(voltage_at_time["Current"].mean())
+    t = voltage_at_time["Total Time"].iloc[-1] - \
+        voltage_at_time["Total Time"].iloc[0]
+
+    return I*t/3600
+
+
+def q_initial_d():
+    data = extract_step(21, 23, "D", "00")
+
+    # Calculate I and t
     I = abs(data["Current"].mean())
     t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
+
+    # Calculate Q remaining and Q available
     Q_remaining = I*t/3600
-    #print("Remaining charge: "+str(Q_remaining))
+
+    return Q_remaining
+
+def soc_d(test):
+    '''
+    Parameters: test (string) in the form 00, 01, etc..
+
+    Calculates the state of charge of D cell at a given time for the discharge at the end of the test
+    Returns list of SOC values
+    '''
+
+    data = extract_step(21, 24, "D", test)
+
+    # Calculate I and t
+    I = abs(data["Current"].mean())
+    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
+
+    # Calculate Q remaining and Q available
+    Q_remaining = I*t/3600
 
     Q_available = [Q_remaining - I*(data["Total Time"].iloc[i] -
                                     data["Total Time"].iloc[0])/3600 for i in range(len(data["Total Time"]))]
-    #print("Available charge" + str(Q_available))
 
     SOC = [Q_available[i]/Q_remaining for i in range(len(data["Total Time"]))]
+
+    """
     soc_voltage_dict = {
         data["Voltage"].iloc[i]: Q_available[i] / Q_remaining
         for i in range(len(data["Total Time"]))}
-    
-    data_final = {"Total Time": full_t,
-            "Total Voltage": full_voltage, 
-            "Associated SoC": calculate_SoC(full_voltage, SOC)}
+    """
 
-    df = pd.DataFrame(data_final)
-    
-    return df
+    return SOC
 
-def calculate_SoC(full_voltage, thedict):
+
+def soh(test):
+    '''
+    Parameters: test (string) in the form 00, 01, etc..
+
+    Calculates the state of health of a cell at a given time 
+    Returns SOH value of test
+    '''
+
+    data = extract_step(21, 23, "D", test)
+
+    # Calculate I and t
+    I = abs(data["Current"].mean())
+    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
+
+    # Calculate Q remaining
+    Q_remaining = I*t/3600
+    q_initial = q_initial_d()
+
+    SOH = Q_remaining / q_initial
+    print(Q_remaining, q_initial)
+    return SOH
+
+def find_OCV(cell, test):
+    """
+    Parameters: cell (str) C or D, 
+                test (str) in the form of 01, 02, etc...
+                
+    Returns a list [OCV, final_SoC, final_step]
+        OCV: the OCV of the battery at the end of the test
+        SoC: it's associated SoC corresponding to the given OCV
+        step: the step during the OCV
+    """
     
-    associated_soc = []
+    data = extract(cell, test)
     
-    for voltage in full_voltage:
-        if voltage not in thedict:
-            print("Cannot find the SoC of ", voltage)
-            associated_soc.append(0)
-            
+    # Take the Voltage at the end of the test (ie. OCV) and its corresponding test
+    OCV = data["Voltage"].iloc[-1]
+    final_step = data["Step"].iloc[-1]
+    
+    
+    
+    data_full_discharge = extract_step(21, 24, "D", test)
+    voltage_full_discharge = data_full_discharge["Voltage"]
+    
+
+    
+    
+    return [OCV, final_step]
+    
+
+
+
+if __name__ == '__main__':
+    print(find_OCV("D", "03"))
+    
+    plot_test("D", "03")
+    plt.show()
+    
+    
+    """
+    # plt.plot(extract_step(21, 24, "D", "01")["Total Time"], soc_d("01"))
+    data = extract("D", "00")
+    # print(data.loc[(data["Total Time"] > 142460) & (data["Total Time"] < 142480)])
+    # plot_test("D", "01")
+    # plot_test("D","13")
+    soh_s = []
+    for i in range(0, 13+1):
+        if i < 10:
+            soh_s.append(soh("0"+str(i)))
         else:
-            return None
-
-# if we then associate a State of Charge to voltage, it should be okay right, because there is a direct relationship between the OCV and SoC
-#data = main.extract_step(21, 23, "D", "01")["Voltage"]
-#SOC_alpha = [soc_d("01")[1][voltage] for voltage in data]
-
-
+            soh_s.append(soh(str(i)))
+    plt.plot(list(range(0, 13+1)), soh_s)
+    plt.show()
+    """
