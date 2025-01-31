f5dbc4e08f528f99c28d776e6ce12daf9680b95e
Kuroda Akira <akirakuroda47@gmail.com>
2025-01-31 09:24:54 +0100

    Curve fitting pt. 1

--- plot_test_ak.py
+++ plot_test_ak.py
@@ -1,320 +1,56 @@
-import pandas as pd
 import numpy as np
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
-import os
 
-from cell_plotting_tests_etienne import *
 
-directory = f"./cells_data"
 
-# extracts all csv file names in directory
-csv_files = [f for f in os.listdir(directory)]
+# Define the model function
 
-# extract function to extract all data
+def constant_model(x, a):
+    return a
 
+def linear_model(x, a, b):
+    return a * x + b
 
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
+def quadratic_model(x, a, b, c):
+    return a * x ** 2 + b * x + c
 
-def plot_test(cell, test):
-    '''
-    Parameters:
-        cell (string) C or D,
-        test (string) in the form 00, 01, etc..
+def cubic_model(x, a, b, c, d):
+    return a * x ** 3 + b * x ** 2 + c * x * d
 
-    Plots voltage, current, and step for given test.
-    Adds a slider to adjust a vertical line dynamically.
+def deg4_model(x, a, b, c, d, e):
+    return a * x ** 4 + b * x ** 3 + c * x ** 2 + d * x + e
 
-    Returns none.
-    '''
+def deg5_model(x, a, b, c, d, e, f):
+    return a * x ** 5 + b * x ** 4 + c * x ** 3 + d * x ** 2 + e * x + f
 
-    plot_data = extract(cell, test)
-    time = plot_data["Total Time"]
-    current = plot_data["Current"]
-    voltage = plot_data["Voltage"]
-    step = plot_data["Step"]
+def deg6_model(x, a, b, c, d, e, f, g):
+    return a * x ** 6 + b * x ** 5 + c * x ** 4 + d * x ** 3 + e * x ** 2 + f * x + g
 
-    fig, axs = plt.subplots(3, 1, figsize=(8, 6))
-    fig.suptitle(f"Cell: {cell.upper()}, Test: {test}")  
+def deg7_model(x, a, b, c, d, e, f, g, h):
+    return a * x ** 7 + b * x ** 6 + c * x ** 5 + d * x ** 4 + e * x ** 3 + f * x ** 2 + g * x + h
 
-    axs[0].plot(time, current, "g")
-    axs[0].set_title("Current")
-    axs[1].plot(time, voltage, "g")
-    axs[1].set_title("Voltage")
-    axs[2].plot(time, step, "g")
-    axs[2].set_title("Step")
+def deg8_model(x, a, b, c, d, e, f, g, h, i):
+    return a * x ** 8 + b * x ** 7 + c * x ** 6 + d * x ** 5 + e * x ** 4 + f * x ** 3 + g * x ** 2 + h * x + i
 
-    # Initial vertical line at the middle of the time range
-    vline_x = time.iloc[len(time) // 2]  
-    vlines = [ax.axvline(x=vline_x, color='r', linestyle='--') for ax in axs]
 
-    # Adjust layout to make space for the slider
-    plt.subplots_adjust(left=0.1, bottom=0.25, right=0.9, top=0.9, hspace=1)
+polynomials = [constant_model, linear_model, quadratic_model, 
+               cubic_model, deg4_model, deg5_model, 
+               deg6_model, deg7_model, deg8_model]
 
-    # Slider axis and control
-    ax_slider = plt.axes([0.2, 0.1, 0.65, 0.03])  # [left, bottom, width, height]
-    slider = Slider(ax_slider, "Time", time.min(), time.max(), valinit=vline_x)
+# Generate some data
+x_data = np.linspace(0, 10, 50)
 
-    def update(val):
-        """ Updates the vertical line when the slider moves """
-        vline_x = slider.val
-        for vline in vlines:
-            vline.set_xdata([vline_x])
-        fig.canvas.draw_idle()  # This ensures the update is shown immediately
+y_data = 2.5 * x_data + 1.0 + np.random.normal(0, 1, 50)
 
-    # Connect slider update function
-    slider.on_changed(update)
+# Fit the model to the data
+params, covariance = curve_fit(linear_model, x_data, y_data)
 
-    plt.show()
+y_data_check = params[0] * x_data + params[1] 
 
-
-def extract(cell, test):
-    '''
-    Parameters : cell (string) C or D, test (string) in the form 00, 01, etc..
-
-    Extracts raw data from csv files corresponding to given cell and test
-    Returns dataframe of extracted data
-    '''
-
-    file = [pd.read_csv(os.path.join(directory, f))
-            for f in csv_files if '_'+(str(cell).upper())+"_" in f and str(test) in f]
-    data = pd.concat(file)  # dataframe of corresponding csv data
-
-    if file == []:
-        print("No test found for given cell and test. Cell entry must be C/c or D/d")
-        return None
-
-    return data
-
-
-def extract_all_steps(first, second, cell, test):
-    '''
-    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
-
-    Returns dataframe for given step interval, does not remove duplicate step sequences
-    '''
-
-    data = extract(cell, test)
-    step_data = data[data["Step"].isin(list(range(first, second+1)))]
-    return step_data
-
-
-def extract_step(first, second, cell, test):
-    '''
-    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
-
-    Returns dataframe for given step interval
-    '''
-
-    data = extract(cell, test)
-    step_data = data[data["Step"].isin(list(range(first, second+1)))]
-
-    # Remove duplicate step sequences
-    step_indices = step_data.index
-    for i in range(1, len(step_indices)):
-        # Check for breaks in the sequence
-        if step_indices[i] != step_indices[i - 1] + 1:
-            # Keep only the first block
-            step_data = step_data.loc[step_indices[:i]]
-            break
-    return step_data
-
-
-def plot_step(first, second, cell, test):
-    '''
-    Parameters: first (int) first step, second (int) second step, cell (string) C or D, test (string) in the form 00, 01, etc..
-
-    Plots voltage current and step for given step interval
-    Returns None
-    '''
-
-    # Extracting data
-    step_data = extract_step(first, second, cell, test)
-    time = step_data["Total Time"]
-    current = step_data["Current"]
-    voltage = step_data["Voltage"]
-    step = step_data["Step"]
-
-    # Plotting
-    fig, axs = plt.subplots(3, 1)
-
-    fig.suptitle("Cell: "+cell.upper()+",test: "+test)  # main title
-
-    axs[0].plot(time, current, "g")
-    axs[0].set_title("Current")
-    axs[1].plot(time, voltage, "g")
-    axs[1].set_title("Voltage")
-    axs[2].plot(time, step, "g")
-    axs[2].set_title("Step")
-
-    plt.subplots_adjust(hspace=1)  # adjust space between plots
-
-    return None
-
-
-def extract_OCV_D():
-    '''
-    USELESS FUNCTION
-
-    Takes average of OCV for each test on cell D
-
-    Returns dictionary of test number and average OCV
-    '''
-    extract_data = {test: 0 for test in range(0, 13+1)}
-    for element in extract_data:
-        print(element)
-        average = 0
-        if element < 10:
-            step_data = extract_all_steps(
-                5, 7, "D", "0"+str(element))[extract_all_steps(5, 7, "D", "0"+str(element))["Step"] == 7]
-            print(step_data)
-        else:
-            step_data = extract_all_steps(5, 7, "D", str(element))[
-                extract_all_steps(5, 7, "D", str(element))["Step"] == 7]
-        for value in step_data["Voltage"]:
-            average += value
-        extract_data[element] = average/len(step_data["Voltage"])
-    return extract_data
-    '''
-    extract_data = {test:0 for test in range(0,13+1)}
-    for element in extract_data:
-        step_data = extract_step(5, 7, "D", element)
-        extract_data[element] = step_data["Voltage"].iloc[-1]
-    return extract_data
-    '''
-
-
-def q_initial_d_precise():
-    '''
-    Returns initial charge for D cell
-    '''
-    voltage_at_time = extract("D", "00")[(extract("D", "00")[
-        "Total Time"] >= 123658.7) & (extract("D", "00")["Total Time"] <= 142474.6)]
-    I = abs(voltage_at_time["Current"].mean())
-    t = voltage_at_time["Total Time"].iloc[-1] - \
-        voltage_at_time["Total Time"].iloc[0]
-
-    return I*t/3600
-
-
-def q_initial_d():
-    data = extract_step(21, 23, "D", "00")
-
-    # Calculate I and t
-    I = abs(data["Current"].mean())
-    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
-
-    # Calculate Q remaining and Q available
-    Q_remaining = I*t/3600
-
-    return Q_remaining
-
-def soc_d(test):
-    '''
-    Parameters: test (string) in the form 00, 01, etc..
-
-    Calculates the state of charge of D cell at a given time for the discharge at the end of the test
-    Returns list of SOC values
-    '''
-
-    data = extract_step(21, 24, "D", test)
-
-    # Calculate I and t
-    I = abs(data["Current"].mean())
-    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
-
-    # Calculate Q remaining and Q available
-    Q_remaining = I*t/3600
-
-    Q_available = [Q_remaining - I*(data["Total Time"].iloc[i] -
-                                    data["Total Time"].iloc[0])/3600 for i in range(len(data["Total Time"]))]
-
-    SOC = [Q_available[i]/Q_remaining for i in range(len(data["Total Time"]))]
-
-    """
-    soc_voltage_dict = {
-        data["Voltage"].iloc[i]: Q_available[i] / Q_remaining
-        for i in range(len(data["Total Time"]))}
-    """
-
-    return SOC
-
-
-def soh(test):
-    '''
-    Parameters: test (string) in the form 00, 01, etc..
-
-    Calculates the state of health of a cell at a given time 
-    Returns SOH value of test
-    '''
-
-    data = extract_step(21, 23, "D", test)
-
-    # Calculate I and t
-    I = abs(data["Current"].mean())
-    t = data["Total Time"].iloc[-1]-data["Total Time"].iloc[0]
-
-    # Calculate Q remaining
-    Q_remaining = I*t/3600
-    q_initial = q_initial_d()
-
-    SOH = Q_remaining / q_initial
-    print(Q_remaining, q_initial)
-    return SOH
-
-
-def find_OCV_c(cell, test):
-    data = extract(cell, test)[extract(cell,test)["Step"] == 5]
-    data_no_dupes = data.loc[~(data["Total Time"].diff().abs() < 3600)]
-    print(data_no_dupes)
-    return data_no_dupes
-
-
-
-def find_OCV_d(cell, test):
-    """
-    Parameters: cell (str) C or D, 
-                test (str) in the form of 01, 02, etc...
-                
-    Returns a list of different times that the circuit has reached OCV
-    """
+# Output the parameters
+print("Fitted parameters:", params)
+plt.plot(x_data, y_data, "ro")
+plt.plot(x_data, y_data_check, "b--")
+plt.show()
     
-    data = extract(cell, test)[extract(cell,test)["Current"] == 0]
-    data_no_dupes = data.loc[~(data["Total Time"].diff().abs() < 3600)]
-    print(data_no_dupes)
-    return data_no_dupes
-
-    
-
-
-if __name__ == '__main__':  
-      
-
-    #find_OCV_d("D", "03")
-    plot_test("D", "03")
-    plt.show()
-    
-    
-    """
-    print(len(soc_full_c("05")))
-    print(len(extract("C", "05")["Total Time"]))
-    
-
-    
-
-    
-    # plt.plot(extract_step(21, 24, "D", "01")["Total Time"], soc_d("01"))
-    data = extract("D", "00")
-    # print(data.loc[(data["Total Time"] > 142460) & (data["Total Time"] < 142480)])
-    # plot_test("D", "01")
-    # plot_test("D","13")
-    soh_s = []
-    for i in range(0, 13+1):
-        if i < 10:
-            soh_s.append(soh("0"+str(i)))
-        else:
-            soh_s.append(soh(str(i)))
-    plt.plot(list(range(0, 13+1)), soh_s)
-    plt.show()
-    """
