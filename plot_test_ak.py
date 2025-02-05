8479af75f73b67f6267111ae783e0d2ca33d905b
Kuroda Akira <akirakuroda47@gmail.com>
2025-02-05 14:21:06 +0100

    graph soc ocv and R0 formula

--- plot_test_ak.py
+++ plot_test_ak.py
@@ -5,16 +5,54 @@ import plot as pt
 import data as dt
 import OCV_fit
 
-cell = "D"
+
+
+cell = "C"
 test = "08"
 
-pt.plot_soc_ocv(cell, test)
-SoH = dt.soh(cell, test)
+def model_data_soc_ocv(cell, test):
+    
+    pt.plot_soc_ocv(cell, test)
 
-df = dt.soc_ocv(cell, test)
-OCV_estimated = [OCV_fit.f(soc,SoH) for soc in df["SoC"]]
+    polynomial = dt.soc_ocv_fitted(cell, test)
 
-df = df.sort_values(by="SoC", ascending = False)
+    x = np.linspace(0, 1, 100)
 
-plt.plot(df["SoC"], OCV_estimated)
+    plt.plot(x, polynomial(x), "r--")
+
+"""
+model_data_soc_ocv(cell, test)
 plt.show()
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def plot_soc_var(cell, test, var):
+    if var == "R0":
+        dt.add_R0(cell, test)
+    elif var == "OCV":
+        dt.plot_soc_ocv(cell, test)
+        
+plot_soc_var(cell, test, "R0")
+plt.show()
+
+
+def model_data_soc_var(cell, test, var):
+    
+    plot_soc_var(cell, test, var)
+    
+        
+            
+    
+
