c81513ac3d6032f9ce4098ad6551ef15e829d0b9
Kuroda Akira <akirakuroda47@gmail.com>
2025-02-06 09:41:21 +0100

    oomba goomba

--- plot_test_ak.py
+++ plot_test_ak.py
@@ -12,15 +12,23 @@ test = "0"
 
 def model_data_soc_ocv(cell, test):
     
-    pt.plot_socx_ocv(cell, test)
+    pt.plot_soc_ocv(cell, test)
 
     polynomial = dt.soc_ocv_fitted(cell, test)
 
     x = np.linspace(0, 1, 100)
 
-    plt.plot(x, polynomial(x), "b-")
+    plt.plot(x, polynomial(x))
+
+def model_data_soc_ocv_soh(cell):
+    
+    model_data_soc_ocv(cell, 0)
+    model_data_soc_ocv(cell, 5)
+    model_data_soc_ocv(cell, 11)
 
 
 
-pt.plot_soh_cell_c_cell_d()
+model_data_soc_ocv_soh("C")
+                       
 plt.show()
+
